47f067037733f266fc131330f5bf2c6f918fe909
Praise Jacob <iampraisejacob@gmail.com>
2026-03-01 04:20:51 +0530

    refactor(packages/contractor): add client side builder and default to ZodRawShape instead of Record<string, never>

--- packages/contractor/src/client.ts
+++ packages/contractor/src/client.ts
@@ -2,6 +2,13 @@ import type { Contract } from "./types/Contract";
 import z from "zod";
 import { defineContract } from "./defineContract";
 
+type BaseContract = Contract<
+    z.ZodRawShape,
+    z.ZodRawShape,
+    z.ZodRawShape,
+    z.ZodRawShape
+>;
+
 type InferPayload<T> =
     T extends Contract<infer TBody, infer TQuery, infer TParams, z.ZodRawShape>
         ? {
@@ -22,7 +29,7 @@ type InferResponse<T> =
         : never;
 
 export type ClientSchema<T> = {
-    [K in keyof T]: T[K] extends Contract
+    [K in keyof T]: T[K] extends BaseContract
         ? (payload: InferPayload<T[K]>) => Promise<InferResponse<T[K]>>
         : T[K] extends object
           ? ClientSchema<T[K]>
@@ -38,26 +45,49 @@ const contractSchema = z.object({
     route: z.string(),
     payload: z
         .object({
-            body: z.object(),
-            params: z.object(),
-            query: z.object(),
+            body: z.object().optional(),
+            params: z.object().optional(),
+            query: z.object().optional(),
         })
         .optional(),
     response: z.object().optional(),
 });
 
 interface SchemaNode {
-    [key: string]:
-        | SchemaNode
-        | Contract<z.ZodRawShape, z.ZodRawShape, z.ZodRawShape, z.ZodRawShape>;
+    [key: string]: SchemaNode | BaseContract;
 }
 
-function isContract(schema: SchemaNode | Contract): schema is Contract {
+function isContract(
+    schema: SchemaNode | BaseContract,
+): schema is Contract<
+    z.ZodRawShape,
+    z.ZodRawShape,
+    z.ZodRawShape,
+    z.ZodRawShape
+> {
     return contractSchema.safeParse(schema).success;
 }
 
-function buildSchema<T extends SchemaNode>(schema: T) {
-    return {} as ClientSchema<T>;
+async function fetchClient<T extends Contract>(
+    contract: T,
+    payload: InferPayload<T>,
+): Promise<InferResponse<T>> {}
+
+function buildSchema<T extends SchemaNode>(schema: T): ClientSchema<T> {
+    return Object.fromEntries(
+        Object.entries(schema).map(([key, value]) => {
+            if (isContract(value)) {
+                return [
+                    key,
+                    (payload: InferPayload<typeof value>) => {
+                        return fetchClient(value, payload);
+                    },
+                ];
+            } else {
+                return [key, buildSchema(value)];
+            }
+        }),
+    );
 }
 
 const schema = {
@@ -76,4 +106,4 @@ const schema = {
 };
 
 const client = buildSchema(schema);
-client.test({ body: { data: "felix" } });
+client.test({});
