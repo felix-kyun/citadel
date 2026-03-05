a1e1a3173a39d6ac4024f6de98ef751e76cfc718
Praise Jacob <iampraisejacob@gmail.com>
2026-03-05 16:39:59 +0530

    refactor(packages/contractor): add a type for function used by client

--- packages/contractor/src/types/utils.ts
+++ packages/contractor/src/types/utils.ts
@@ -2,23 +2,29 @@ import type { Contract } from "./Contract";
 import type z from "zod";
 
 export type FilterNever<T> = {
-	[K in keyof T as T[K] extends never ? never : K]: T[K];
+    [K in keyof T as T[K] extends never ? never : K]: T[K];
 };
 
 export type InferPayload<T> =
-	T extends Contract<infer TBody, infer TQuery, infer TParams, z.ZodType>
-		? {
-				body: z.infer<TBody>;
-				query: z.infer<TQuery>;
-				params: z.infer<TParams>;
-			}
-		: never;
+    T extends Contract<infer TBody, infer TQuery, infer TParams, z.ZodType>
+        ? {
+              body: z.infer<TBody>;
+              query: z.infer<TQuery>;
+              params: z.infer<TParams>;
+          }
+        : never;
 
 export type InferResponse<T> =
-	T extends Contract<z.ZodType, z.ZodType, z.ZodType, infer TResponse>
-		? z.infer<TResponse>
-		: never;
+    T extends Contract<z.ZodType, z.ZodType, z.ZodType, infer TResponse>
+        ? z.infer<TResponse>
+        : never;
 
 export interface ClientOptions {
-	baseUrl: string;
+    baseUrl: string;
 }
+
+export type Clientfn<TContract extends Contract> = (
+    contract: TContract,
+    payload: InferPayload<TContract>,
+    options: ClientOptions,
+) => Promise<InferResponse<TContract>>;
